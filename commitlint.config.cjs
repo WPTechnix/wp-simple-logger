9e0b780f51520fa7f9ece51213679f37d96c70a7
owaisahmed5300 <owaisahmed5300@gmail.com>
2026-07-16 12:54:45 +0500

    ci: remove Dependabot-era commitlint workarounds now that Renovate handles updates

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,3 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  ignores: [
-    (message) =>
-      /^(?:chore(?:\([^)]+\))?:\s*)?bump\b/i.test(message) ||
-      /^Bumps?\b/i.test(message),
-  ],
 };
